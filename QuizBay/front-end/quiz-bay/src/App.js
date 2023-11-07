c32a7937ccdd1a6d2342cf379b191046332dd464
Farzana S <farzanashaju16@gmail.com>
2023-11-07 15:47:04 +0530

    Update App.js and Question.js component

--- QuizBay/front-end/quiz-bay/src/App.js
+++ QuizBay/front-end/quiz-bay/src/App.js
@@ -9,20 +9,34 @@ class App extends Component {
       questions: [],
       currentQuestionIndex: 0,
       score: 0,
+      isLoggedIn: false,
+      isQuizStarted: false,
     };
   }
 
   async componentDidMount() {
     try {
-      const response = await axios.get('https://api.supabase.io/rest/v1/tables/questions');
-      const { data } = response.data;
+      const response = await axios.get('https://isclssxahgldembekjxw.supabase.co/rest/v1/quizzes');
+      const questions = response.data;
       this.setState({
-        questions: data,
+        questions: questions,
       });
     } catch (error) {
       console.error('Error fetching quiz questions:', error);
     }
   }
+
+  handleLogin = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+  };
+
+  handleStartQuiz = () => {
+    this.setState({
+      isQuizStarted: true,
+    });
+  };
 
   handleAnswer = (selectedOption, correctAnswer) => {
     const { currentQuestionIndex, questions, score } = this.state;
@@ -33,23 +47,47 @@ class App extends Component {
       });
     }
 
-    this.setState({
-      currentQuestionIndex: currentQuestionIndex + 1,
-    });
+    if (currentQuestionIndex === questions.length - 1) {
+      this.setState({
+        isQuizStarted: false,
+      });
+    } else {
+      this.setState({
+        currentQuestionIndex: currentQuestionIndex + 1,
+      });
+    }
   };
 
   render() {
-    const { questions, currentQuestionIndex, score } = this.state;
+    const { questions, currentQuestionIndex, score, isLoggedIn, isQuizStarted } = this.state;
     const currentQuestion = questions[currentQuestionIndex];
 
     return (
       <div className="quiz-app">
-        {currentQuestion ? (
-          <Question question={currentQuestion} handleAnswer={this.handleAnswer} />
+        {!isLoggedIn ? (
+          <div className="login">
+            <h2>Welcome to the Quiz App!</h2>
+            <button onClick={this.handleLogin}>Sign Up / Login</button>
+          </div>
         ) : (
-          <div className="result">
-            <h2>Quiz Completed!</h2>
-            <p>Your Score: {score}/{questions.length}</p>
+          <div className="quiz-interface">
+            {!isQuizStarted ? (
+              <div className="start-quiz">
+                <h2>Quiz Interface</h2>
+                <button onClick={this.handleStartQuiz}>Start Quiz</button>
+              </div>
+            ) : (
+              <div className="question-container">
+                {currentQuestion ? (
+                  <Question question={currentQuestion} handleAnswer={this.handleAnswer} />
+                ) : (
+                  <div className="result">
+                    <h2>Quiz Completed!</h2>
+                    <p>Your Score: {score}/{questions.length}</p>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         )}
       </div>
