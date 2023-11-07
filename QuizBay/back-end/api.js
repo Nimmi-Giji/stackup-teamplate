5970fa4f5e812dea5d6b48729fccb15be9f8de5f
Nimmi <134776783+Nimmi-Giji@users.noreply.github.com>
2023-11-07 14:37:36 +0530

    Update api.js
    opentdb endpoint

--- QuizBay/back-end/api.js
+++ QuizBay/back-end/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios'); 
 const quizController = require('./quizController');
 
 
@@ -10,5 +11,23 @@ router.get('/quizzes', quizController.getAllQuizzes);
 router.get('/quizzes/:quizId', quizController.getQuizById);
 
 router.post('/quizzes/:quizId/submit', quizController.submitQuizAnswers);
+
+// opentdb
+router.get('/quizzes', async (req, res) => {
+  try {
+    // fetch questions from opentdb
+    const response = await axios.get('https://opentdb.com/api.php', {
+      params: {
+        amount: 10, // number,type subject to change
+        type: 'multiple',
+      },
+    });
+
+    const questions = response.data.results;
+    res.json(questions);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;
